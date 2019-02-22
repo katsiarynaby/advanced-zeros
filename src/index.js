0251eec046dc31c2d1944f6704c1e9730b1bcbe6
Katsiaryna Kananchuk <katsiarynakananchuk@MacBook-Pro-Katsiaryna.local>
2019-02-22 21:27:09 +0300

    current version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var arrayAll = getSimple(base);
+  var arrayUniq = unique(arrayAll);
+  var possibleZeros = test(number, arrayUniq);
+  var countZeros = transform(arrayAll, arrayUniq);
+  var result = lastStep(possibleZeros, countZeros);
+
+
+
+  function getSimple(base) {
+    let array = [];
+    for (i = 2; i < base + 1; i++) {
+      while (base % i == 0) {
+        base /= i;
+        array.push(i);
+      }
+    }
+    return array
+  };
+
+  function unique(array) {
+    var obj = {};
+
+    for (var i = 0; i < array.length; i++) {
+      var str = array[i];
+      obj[str] = true;
+    }
+
+    return Object.keys(obj);
+  }
+
+  function test(number, arrayUniq) {
+    let array = arrayUniq.slice();
+    let zeros = [];
+    for (i = 0; i < array.length; i++) {
+      let count = Math.trunc(number / array[i]);
+      zeros[i] = 0;
+      while (count) {
+        zeros[i] += count;
+        array[i] *= array[i];
+        count = Math.trunc(number / array[i]);
+      }
+    }
+    return zeros
+  }
+
+  function transform(arrayAll, arrayUniq) {
+    let arrayZeros = [];
+    for (i = 0; i < arrayUniq.length; i++) {
+      arrayZeros[i] = arrayAll.filter(number => number == arrayUniq[i]).length;
+    }
+    return arrayZeros
+  }
+
+  function lastStep(possibleZeros, countZeros) {
+    for (i = 0; i < possibleZeros.length; i++) {
+      possibleZeros[i] = Math.trunc(possibleZeros[i] / countZeros[i]);
+      
+    }
+    return Math.min.apply(null, possibleZeros);
+  }
+
+  return result
 }
