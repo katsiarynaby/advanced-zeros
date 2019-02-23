ffbcd72343eae1639ecfa37eda063fdf254b315c
Katsiaryna Kananchuk <katsiarynakananchuk@MacBook-Pro-Katsiaryna.local>
2019-02-23 11:44:51 +0300

    refreshed function names

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 module.exports = function getZerosCount(number, base) {
 
-  var arrayAll = getSimple(base);
-  var arrayUniq = unique(arrayAll);
-  var possibleZeros = test(number, arrayUniq);
-  var countZeros = transform(arrayAll, arrayUniq);
-  var result = lastStep(possibleZeros, countZeros);
+  var arrayAllMn = getSimpleMn(base);
+  var arrayUniqMn = unique(arrayAllMn);
+  var possibleCountZeros = seachPossibleCountZeros(number, arrayUniqMn);
+  var countRepetMn = countRepetMn(arrayAllMn, arrayUniqMn);
+  var zerosCount = zerosCount(possibleCountZeros, countRepetMn);
 
 
-
-  function getSimple(base) {
+// base factorization
+  function getSimpleMn(base) {
     let array = [];
-    for (i = 2; i < base + 1; i++) {
+    for (let i = 2; i < base + 1; i++) {
       while (base % i == 0) {
         base /= i;
         array.push(i);
@@ -19,21 +19,23 @@ module.exports = function getZerosCount(number, base) {
     return array
   };
 
+  //search unique multipliers
   function unique(array) {
     var obj = {};
 
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       var str = array[i];
       obj[str] = true;
     }
 
     return Object.keys(obj);
-  }
+  };
 
-  function test(number, arrayUniq) {
+  //search possible count zeros for each multipliers
+  function seachPossibleCountZeros(number, arrayUniq) {
     let array = arrayUniq.slice();
     let zeros = [];
-    for (i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       let count = Math.trunc(number / array[i]);
       zeros[i] = 0;
       while (count) {
@@ -43,23 +45,25 @@ module.exports = function getZerosCount(number, base) {
       }
     }
     return zeros
-  }
+  };
 
-  function transform(arrayAll, arrayUniq) {
-    let arrayZeros = [];
-    for (i = 0; i < arrayUniq.length; i++) {
-      arrayZeros[i] = arrayAll.filter(number => number == arrayUniq[i]).length;
+  // get count of repetitions for each multiplier
+  function countRepetMn(arrayAll, arrayUniq) {
+    let countRepetMn = [];
+    for (let i = 0; i < arrayUniq.length; i++) {
+      countRepetMn[i] = arrayAll.filter(number => number == arrayUniq[i]).length;
     }
-    return arrayZeros
-  }
+    return countRepetMn
+  };
 
-  function lastStep(possibleZeros, countZeros) {
-    for (i = 0; i < possibleZeros.length; i++) {
-      possibleZeros[i] = Math.trunc(possibleZeros[i] / countZeros[i]);
+  // get zeros count in factorial
+  function zerosCount(possibleCountZeros, countRepetMn) {
+    for (let i = 0; i < possibleCountZeros.length; i++) {
+      possibleCountZeros[i] = Math.trunc(possibleCountZeros[i] / countRepetMn[i]);
       
     }
-    return Math.min.apply(null, possibleZeros);
-  }
+    return Math.min.apply(null, possibleCountZeros);
+  };
 
-  return result
+  return zerosCount
 }
